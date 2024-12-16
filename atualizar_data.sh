fd2d4ee5fb58bf610c5a53cec7f5886fda20f6bf
Jéssica Oliveira <jessicamelestudante@gmail.com>
2024-12-16 14:11:33 -0300

    Atualização Atualização do programa "Atualizar data"

--- atualizar_data.sh
+++ atualizar_data.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Define o caminho do README
 README_PATH="README.md"
-
-# Pega a data e hora atuais no formato desejado (exemplo: 14 de dezembro de 2024)
 DATE=$(date '+%d de %B de %Y, %H:%M')
 
-# Substitui a data no README.md (Assumindo que há um marcador no README.md como "Última modificação: [DATA]")
 sed -i "s/Última modificação:.*/Última modificação: $DATE/" $README_PATH
 
-# Adiciona e faz commit do README.md
 git add $README_PATH
 git commit -m "Atualizar a data da última modificação"
-
-# Agora faz o push para o repositório
 git push origin master
 
-# Adiciona e faz commit do README.md
-git add .
-git commit -m "Atualização $DATE"
+echo "Digite a mensagem de commit"
+read mensagem
 
-# Agora faz o push para o repositório
+git add .
+git commit -m "Atualização $mensagem"
 git push origin master
