9a27e0798460d0c63e713d714970a03b5f6f2549
Jéssica Oliveira <jessicamelestudante@gmail.com>
2024-12-16 13:13:59 -0300

    Atualizar a data da última modificação

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 int main(){
     Sculptor teste(50, 50, 50);
     
-    teste.setColor(0.72, 0, 0, 1.0);
-    teste.putSphere(25,25,25,25);
+    teste.setColor(0.97, 0.65, 0.72, 1.0);
+    teste.putSphere(25, 25, 25, 20);
+    teste.putSphere(10, 25, 25, 5);
+    teste.putSphere(35, 25, 25, 5);
+
 
     //teste.setColor(0.72, 0, 1.0, 1.0);
     //teste.putEllipsoid(50,100,100,25,36,9);
