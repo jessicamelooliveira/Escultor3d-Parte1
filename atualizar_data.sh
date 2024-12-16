591d13565c976df1f0d724f669cb1f067dea6718
Jéssica Oliveira <jessicamelestudante@gmail.com>
2024-12-16 14:13:56 -0300

    Programa modificado para push automático + commit personalizada

--- atualizar_data.sh
+++ atualizar_data.sh
@@ -6,7 +6,7 @@ DATE=$(date '+%d de %B de %Y, %H:%M')
 sed -i "s/Última modificação:.*/Última modificação: $DATE/" $README_PATH
 
 git add $README_PATH
-git commit -m "Atualizar a data da última modificação"
+git commit -m "Atualização da data da última modificação"
 git push origin master
 
 echo "Digite a mensagem de commit"
