cf161884f7d5eec875939d6d63bbd950197c2c8b
Jéssica Oliveira <jessicamelestudante@gmail.com>
2024-12-17 19:55:31 -0300

    Escultura atualizada.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(){
     kirby.putSphere(20, 40, 25, 10);
     kirby.putSphere(70, 40, 25, 10);
 
-    // Pes
+    // Pés
     kirby.setColor(0.86, 0, 0.34, 1.0);
     kirby.putEllipsoid(28, 20, 25, 15, 5, 12);
     kirby.putEllipsoid(62, 20, 25, 15, 5, 12);
@@ -20,23 +20,13 @@ int main(){
 
     // Olhos
     kirby.setColor(0, 0, 0, 1.0);
-    for(int i=38; i<53; i++){
-        if(i>=40 && i<=50) continue;
-        for(int j=45; j<55; j++){
-        kirby.cutVoxel(i, j, 49);
-        }
-    }
-    for(int i=37; i<54; i++){
-        if(i>=40 && i<=50) continue;
-        for(int j=45; j<55; j++){
-            kirby.putVoxel(i, j, 48);
-        }
-    }
+    kirby.cutBox(38, 39, 45, 54, 49, 49);
+    kirby.cutBox(51, 52, 45, 54, 49, 49);
+    kirby.putBox(38, 39, 45, 54, 48, 48);
+    kirby.putBox(51, 52, 45, 54, 48, 48);
 
     // Boca
-    for(int i=44; i<47; i++){
-        kirby.putVoxel(i, 41, 49);
-    }
+    kirby.putBox(44, 46, 41, 41, 49, 49);
     kirby.putVoxel(43, 42, 49);
     kirby.putVoxel(47, 42, 49);
     kirby.putVoxel(42, 43, 49);
@@ -44,12 +34,19 @@ int main(){
 
     // Brilho
     kirby.setColor(1.0, 1.0, 1.0, 1.0);
-    for(int i=38; i<54; i++){
-        if(i>=40 && i<=51) continue;
-        for(int j=53; j<55; j++){
-            kirby.putVoxel(i, j, 48);
-        }
-    }
+    kirby.putBox(39, 39, 53, 54, 48, 48);
+    kirby.putBox(52, 52, 53, 54, 48, 48);
 
+    // Bochechas
+    kirby.setColor(0.95, 0.44, 0.52, 1.0);
+    kirby.cutBox(36, 36, 42, 43, 49, 49);
+    kirby.cutBox(53, 53, 42, 43, 49, 49);
+    kirby.putBox(34, 36, 42, 43, 48, 48);
+    kirby.putBox(53, 55, 42, 43, 48, 48);
+
+    // Aparando o que não é necessário
+    kirby.cutBox(0, 99, 0, 99, 50, 50);
+
+    // Escrevendo o arquivo OFF
     kirby.writeOFF("kirby.off");
 }
